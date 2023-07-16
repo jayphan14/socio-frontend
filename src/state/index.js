cb6071a0941d526762b3cbdb73b8ac2cdf194921
jayphan14 <kha21052004@gmail.com>
2023-07-16 15:06:02 -0400

    redux set up

--- src/state/index.js
+++ src/state/index.js
@@ -1,1 +1,49 @@
-import 
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state, action) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("no friends found");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+
+    // update the list of posts: newPost is in action.payload.post_ids
+    setPost: (state, action) => {
+      updatedPosts = state.posts.map((p) => {
+        if (p._id === action.payload.post_id) return action.payload.post;
+        return p;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducers;
